1e463265652f0fed4f9f810775a98cbe52a5c44b
manh12a5 <manh12a5@gmail.com>
2021-11-05 17:00:39 +0700

    Fix jenkinsfile

--- src/test/java/com/example/calculator/BasicTest.java
+++ src/test/java/com/example/calculator/BasicTest.java
@@ -40,7 +40,7 @@ public class BasicTest {
         String leftNumber = "2";
         String rightNumber = "2";
         String operator = "*";
-        double result = 5;
+        double result = 4;
         assertEquals(calculator.calculateResult(leftNumber, rightNumber, operator), result);
     }
 
